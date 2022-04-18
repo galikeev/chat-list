a9efe8e8f244155f0f0adf17407c224e9afbf763
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-18 22:25:30 +0500

    rewrited to js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {PageIndex} from "./pages/index";
+import PageIndex from "./pages/index/index";
 
 function App() {
+
   return (
     <div className="App">
       <PageIndex />
---
+++ src/pages/index/index.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+import { getChatList, getMessagesList } from "../../api/chat";
+
+import ChatList from "../../components/chatList/ChatList";
+import Header from "../../components/header/Header";
+import MessagesList from "../../components/messagesList/MessagesList";
+
+
+const PageIndex = () => {
+
+    const [chatList, setChatList] = useState([]);
+    const [messages, setMessages] = useState([]);
+    const [header, setHeader] = useState('');
+
+    useEffect(() => {
+        getChatList()
+            .then(onChatListLoaded)
+    }, [])
+    
+    const onChatListLoaded = (chatList) => {
+        setChatList(() => [...chatList]);
+    }
+    
+    const onChatSelected = (id) => {
+        const chat = chatList.find(elem => elem.id === id);
+        setHeader(chat.title);
+        getMessagesList(chat.id)
+            .then(onMessagesLoaded)
+    }
+
+    const onMessagesLoaded = (messages) => {
+        setMessages(() => [...messages]);
+    }
+
+    return (
+        <div className="page">
+            <ChatList chatList={chatList} onChatSelected={onChatSelected}/>
+            <div className="page__wrapper">
+                <Header header={header}/>
+                <MessagesList messages={messages}/>
+            </div>
+        </div>
+    )
+}
+
+export default PageIndex;

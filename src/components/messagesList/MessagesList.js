b04d751aae8ab3167a18ab76d55afd8f2888d385
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-19 19:18:03 +0500

    fixed time

--- src/components/messagesList/MessagesList.js
+++ src/components/messagesList/MessagesList.js
@@ -1,15 +1,34 @@
+import dayjs from "dayjs";
+
+import { ReadMessage } from "../../assets";
 import './messagesList.scss';
 
 const MessagesList = ({messages}) => {
 
     const renderElems = messages.map(elem => {
-        return elem.user.you ? 
-            <div className="message__item message__item_you">
+
+        const time = dayjs.unix(elem.created_at).format('HH:mm');
+
+        return (
+            elem.user.you ? 
+            <div key={elem.id} className="message__item message__item_you">
                 {elem.message}
+                <div className="message__time">
+                    <div>{time}</div>
+                    <img src={ReadMessage} alt="icon"/>
+                </div>
             </div> :
-            <div className="message__item message__item_notyou">
-                {elem.message}
+            <div key={elem.id} className="message__wrapper">
+                <img src={elem.user.avatar} alt={elem.user.name} className="message__img"/>
+                <div className="message__block">
+                    {elem.user.name} {elem.user.surname}
+                    <div className="message__item message__item_notyou">
+                        {elem.message}
+                        <div className="message__time">{time}</div>
+                    </div>
+                </div>
             </div>
+        )
     })
 
     return (

a49b3123af5a48f8232af83c3bfcbaf6ab1f0a25
Ramil Galikeev <galikeevramil95@gmail.com>
2022-04-20 15:48:32 +0500

    rewrited to redux

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,46 +1,48 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-import { getChatList, getMessagesList } from "../../api/chat";
+import ChatListService from '../../api/chat/index';
 
 import ChatList from "../../components/chatList/ChatList";
 import Header from "../../components/header/Header";
 import MessagesList from "../../components/messagesList/MessagesList";
+import Spinner from '../../components/spinner/Spinner';
 
 
 const PageIndex = () => {
 
-    const [chatList, setChatList] = useState([]);
-    const [messages, setMessages] = useState([]);
     const [header, setHeader] = useState('');
+
+    const {getChatList, getMessagesList} = ChatListService();
+
+    const {
+        chats,
+        statusChats,
+        statusMessages,
+        messages
+    } = useSelector(state => state);
 
     useEffect(() => {
         getChatList()
-            .then(onChatListLoaded)
-    }, [])
-    
-    const onChatListLoaded = (chatList) => {
-        setChatList(() => [...chatList]);
-    }
+    }, []);
     
     const onChatSelected = (id) => {
-        const chat = chatList.find(elem => elem.id === id);
+        const chat = chats.find(elem => elem.id === id);
         setHeader(chat.title);
         getMessagesList(chat.id)
-            .then(onMessagesLoaded)
-    }
-
-    const onMessagesLoaded = (messages) => {
-        setMessages(() => [...messages]);
     }
 
     return (
         <div className="page">
-            <ChatList 
-                chatList={chatList} 
-                onChatSelected={onChatSelected}/>
+            <div className="page__left">
+                <h3 className="chats__title">All chats</h3>
+                {statusChats === 'loading' ? <Spinner/> : <ChatList 
+                    chatList={chats} 
+                    onChatSelected={onChatSelected}/>}
+            </div>
             <div className="page__wrapper">
                 <Header header={header}/>
-                <MessagesList messages={messages}/>
+                {statusMessages === 'loading' ? <Spinner/> : <MessagesList messages={messages}/>}
             </div>
         </div>
     )
